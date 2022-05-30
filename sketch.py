4fdc1d5219039e251557ff96b9dd7278445de1db
Mohit Sharma <mohit13@outlook.com>
2022-05-30 03:23:33 +0200

    media pipe pose estimation added

--- sketch.py
+++ sketch.py
@@ -6,10 +6,15 @@ import cv2
 import numpy as np
 import mediapipe as mp
 
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_holistic = mp.solutions.holistic
+
 
 # initialize mediapipe 
 mp_selfie_segmentation = mp.solutions.selfie_segmentation
 selfie_segmentation = mp_selfie_segmentation.SelfieSegmentation(model_selection=1)
+
 
 # store background images in a list
 image_path = os.getcwd()  + '\images'
@@ -96,18 +101,59 @@ def changebackground(frame, bg_image):
 
     return output_image
 
+def holisticeatimate(image):
+    
+    
+    with mp_holistic.Holistic(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as holistic:
+    
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        image.flags.writeable = False
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = holistic.process(image)
+
+        # Draw landmark annotation on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        mp_drawing.draw_landmarks(
+            image,
+            results.face_landmarks,
+            mp_holistic.FACEMESH_CONTOURS,
+            landmark_drawing_spec=None,
+            connection_drawing_spec=mp_drawing_styles
+            .get_default_face_mesh_contours_style())
+        mp_drawing.draw_landmarks(
+            image,
+            results.pose_landmarks,
+            mp_holistic.POSE_CONNECTIONS,
+            landmark_drawing_spec=mp_drawing_styles
+            .get_default_pose_landmarks_style())
+        # Flip the image horizontally for a selfie-view display.
+        
+    return cv2.flip(image, 1)
+
 # create videocapture object to access the webcam
 cap = cv2.VideoCapture(0)
 
 while cap.isOpened():
-    _, frame = cap.read()
+    success, image = cap.read()
+    if not success:
+      print("Ignoring empty camera frame.")
+      # If loading a video, use 'break' instead of 'continue'.
+      continue
 
-    cartoonoutput = cartonify(frame)
+    cartoonoutput = cartonify(image)
     output1 = changebackground(cartoonoutput, bg_image)
     
-    output2 = img2sketch(frame, k_size=151)
-    
-    output = np.concatenate((output1, output2, frame), axis = 0)
+    output2 = img2sketch(image, k_size=151)
+
+    output3 = holisticeatimate(image)
+
+    outputa = np.concatenate((output1, output2), axis = 0)
+    outputb = np.concatenate((output3, image), axis = 0)
+    output = np.concatenate((outputa, outputb), axis = 1)
 
     # show outputs
     cv2.imshow("Output", output)
